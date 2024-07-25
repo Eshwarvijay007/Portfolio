85cace565e52514ed78f72aeefa02f5d703e40ce
Eshwarvijay007 <eshwarvijaynallur@gmail.com>
2024-07-25 22:10:46 +0530

    updated files

---
+++ src/constants.js
@@ -0,0 +1,33 @@
+
+export const services = [
+    {
+        title: "AI intern",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    
+    
+];
+
+export const name = 'Eshwar';
+
+export const experiences = [
+    {
+        'company': 'Aqmenz Automation and private limited',
+        
+        'duration': 'April 2023 - Present',
+        'logo': 'https://images.crunchbase.com/image/upload/c_pad,f_auto,q_auto:eco,dpr_1/e5gz1yggdy8sc1umna3e',
+        'points': [
+            'Involved in dynamic contributions to web and mobile📱 development projects, actively influencing and improving project outcomes.',
+            'Developed multiple shared components to simplify tasks for fellow developers, boosting efficiency and teamwork.',
+            'Enhanced product performance through significant contributions to backend development, optimizing essential components for improved efficiency 📈.',
+            'Implemented data security for the company’s product to provide role-based access control ⚙️.'
+        ],
+    },
+    
+]
+
+
+
+export const EMAIL_JS_SERVICE_ID = 'service_90wb9t5';
+export const EMAIL_JS_TEMPLATE_ID = 'template_8l87rew';
+export const EMAIL_JS_PUBLIC_KEY = 'pHPjc9RGGkF66r86N';
